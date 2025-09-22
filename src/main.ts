08b0a5e2c4bd0d2077e1e19095ebcd4e389f8a93
martinhamel <hamel.martin75@gmail.com>
2025-09-22 18:16:05 +0200

    ok

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { EventModule } from './event.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(EventModule);
-  await app.listen(8080);
+  const port = process.env.PORT || 8080;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
