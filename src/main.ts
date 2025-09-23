2d6d97ae791859f8fe0205d206d65daea260402e
martinhamel <hamel.martin75@gmail.com>
2025-09-23 13:30:26 +0200

    ok

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { EventModule } from './event.module';
 
+const PORT = process.env.PORT ?? 8080
+
 async function bootstrap() {
   const app = await NestFactory.create(EventModule);
-  await app.listen(8080);
+  await app.listen(PORT);
+  console.log("Server running on localhost://"+PORT);
 }
 bootstrap();
