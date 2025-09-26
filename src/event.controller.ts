8ab7121c4225d39ec7efa928c3f8269b588b0907
martinhamel <hamel.martin75@gmail.com>
2025-09-26 15:16:42 +0200

    ok

--- src/event.controller.ts
+++ src/event.controller.ts
@@ -2,56 +2,47 @@ import { Controller, Get, Post, Body, Param, Delete, Query, HttpCode } from '@ne
 import { EventService } from './event.service';
 import { Event } from './Event';
 
-@Controller()
+@Controller('events') // Préfixe pour toutes les routes de ce contrôleur
 export class EventController {
   constructor(private readonly eventService: EventService) {}
 
-@Post('event')
-addEvent(@Body() event:Event):string
-{
+  @Post()
+  addEvent(@Body() event: Event): string {
     this.eventService.addEvent(event);
-    return "Evènement ajouté avec succès !"
+    return "Événement ajouté avec succès !";
+  }
 
-}
-
-@Get('list')
+  @Get('list')
   async listAllEvents(): Promise<Partial<Event>[]> {
     return this.eventService.getAllEvents();
   }
 
-@Get()
-getEventByCodePostal(@Query('code_postal') code_postal?: number): Partial<Event>[] {
-    if (code_postal)
-    {
+  @Get()
+  getEventByCodePostal(@Query('code_postal') code_postal?: number): Partial<Event>[] {
+    if (code_postal) {
       return this.eventService.getEventByCodePostal(code_postal);
     }
     return this.eventService.getAllEvents();
   }
 
-@Get(':objectid')
-getEvent(@Param('objectid') objectid: number):Event | undefined
-{
+  @Get(':objectid')
+  getEvent(@Param('objectid') objectid: number): Event | undefined {
     return this.eventService.getEvent(objectid);
-}
+  }
 
+  @Delete(':objectid')
+  removeEvent(@Param('objectid') objectid: number) {
+    this.eventService.removeEvent(objectid);
+    return { message: `Événement ${objectid} supprimé avec succès.` };
+  }
 
-@Delete(':objectid')
-removeEvent(@Param('objectid') objectid: number)
-{
-    return this.eventService.removeEvent(objectid);
-}
-
-@Get(':categorie')
-getTotalNumberOfEventsByCat(@Query('categorie') categorie?: string): Partial<Event>[]
-{
-  if(categorie)
-  {
+  @Get('category/:categorie')
+  getTotalNumberOfEventsByCat(@Param('categorie') categorie: string): Partial<Event>[] {
     return this.eventService.getTotalNumberOfEventsByCat(categorie);
   }
-  return this.eventService.getAllEvents();
-}
-@Post('search')
-@HttpCode(200)
+
+  @Post('search')
+  @HttpCode(200)
   searchEvent(@Body() { term }: { term: string }): Event[] {
     return this.eventService.search(term);
   }
